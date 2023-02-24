a1107b106f54679f02716a203419da6df52befcd
gayathrimurugeshan <125565704+gayathrimurugeshan@users.noreply.github.com>
2023-02-24 17:05:08 +0000

    end

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import TodoList from './component/TodoList';
 import 'bootstrap/dist/css/bootstrap.min.css';
--- todo/src/component/TodoList.js
+++ todo/src/component/TodoList.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
+import CreateTask from '../modals/CreateTask';
 
 const TodoList = () => {
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => {
+        setModal(modal);
+
+
+    }
+
+
     return (
         <>
             <div className='header text-center'>
             <h3> Todo List</h3>
-            <button className = 'btn btn-primary'>create</button>
+            <button className = 'btn btn-primary' onClick={ () => setModal(true)}>create</button>
             </div>
+            <CreateTask toggle = {toggle} model = {modal}/>
 
         </>
     );
